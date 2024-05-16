5f555e50ff506183d04358424a35b628753f8aba
Langston Nashold <lgnashold@gmail.com>
2024-05-15 22:29:04 -0700

    v15

--- script.py
+++ script.py
@@ -6,45 +6,50 @@ from vals.sdk.sdk import patch, run_evaluations
 
 print(os.listdir())
 
+
 print("Cur dir", os.path.curdir)
 
-# from vals_entry import vals_entry_function
+import sys
 
-# # Replace with your own test suite
-# SUITE_URL = "https://dev.platform.vals.ai/view?test_suite_id=38ed6d4b-4714-4630-a001-16238c16fc8b"
+sys.path.append(os.getcwd())
 
-# client = patch(OpenAI(api_key=os.environ.get("OPEN_AI_KEY")))
+from vals_entry import vals_entry_function
 
-# # Run the test suite
-# run_id = run_evaluations(
-#     SUITE_URL,
-#     vals_entry_function,
-#     verbosity=0,
-# )
+# Replace with your own test suite
+SUITE_URL = "https://dev.platform.vals.ai/view?test_suite_id=38ed6d4b-4714-4630-a001-16238c16fc8b"
 
-# status = wait_for_run_completion(run_id)
-# run_link = get_run_url(run_id)
+client = patch(OpenAI(api_key=os.environ.get("OPEN_AI_KEY")))
 
-# if status == "error":
-#     print(
-#         f"""
-# Vals Eval Results
-# There was an error running the test suite.
+# Run the test suite
+run_id = run_evaluations(
+    SUITE_URL,
+    vals_entry_function,
+    verbosity=0,
+)
 
-#  _View full results at: {run_link}_
-# """
-#     )
-#     exit(1)
+status = wait_for_run_completion(run_id)
+run_link = get_run_url(run_id)
 
-# summary_dict = run_summary(run_id)
+if status == "error":
+    print(
+        f"""
+Vals Eval Results
+There was an error running the test suite.
 
-# print(
-#     f"""
-# Vals Eval Results
-# **Pass Rate**: {summary_dict['passPercentage'] * 100:.2f}%
+ _View full results at: {run_link}_
+"""
+    )
+    exit(1)
 
-# **Error Case Summary**: {summary_dict['textSummary']}
+summary_dict = run_summary(run_id)
 
-# _View full results at: {run_link}_
-# """
-# )
+print(
+    f"""
+Vals Eval Results
+**Pass Rate**: {summary_dict['passPercentage'] * 100:.2f}%
+
+**Error Case Summary**: {summary_dict['textSummary']}
+
+_View full results at: {run_link}_
+"""
+)
