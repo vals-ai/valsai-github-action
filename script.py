f055aa76ec98bc7336df1d731d370c711b3ace16
Langston Nashold <lgnashold@gmail.com>
2024-05-15 21:53:01 -0700

    V12

--- script.py
+++ script.py
@@ -8,43 +8,43 @@ print(os.listdir())
 
 print("Cur dir", os.path.curdir)
 
-from vals_entry import vals_entry_function
+# from vals_entry import vals_entry_function
 
-# Replace with your own test suite
-SUITE_URL = "https://dev.platform.vals.ai/view?test_suite_id=38ed6d4b-4714-4630-a001-16238c16fc8b"
+# # Replace with your own test suite
+# SUITE_URL = "https://dev.platform.vals.ai/view?test_suite_id=38ed6d4b-4714-4630-a001-16238c16fc8b"
 
-client = patch(OpenAI(api_key=os.environ.get("OPEN_AI_KEY")))
+# client = patch(OpenAI(api_key=os.environ.get("OPEN_AI_KEY")))
 
-# Run the test suite
-run_id = run_evaluations(
-    SUITE_URL,
-    vals_entry_function,
-    verbosity=0,
-)
+# # Run the test suite
+# run_id = run_evaluations(
+#     SUITE_URL,
+#     vals_entry_function,
+#     verbosity=0,
+# )
 
-status = wait_for_run_completion(run_id)
-run_link = get_run_url(run_id)
+# status = wait_for_run_completion(run_id)
+# run_link = get_run_url(run_id)
 
-if status == "error":
-    print(
-        f"""
-Vals Eval Results
-There was an error running the test suite.
+# if status == "error":
+#     print(
+#         f"""
+# Vals Eval Results
+# There was an error running the test suite.
 
- _View full results at: {run_link}_
-"""
-    )
-    exit(1)
+#  _View full results at: {run_link}_
+# """
+#     )
+#     exit(1)
 
-summary_dict = run_summary(run_id)
+# summary_dict = run_summary(run_id)
 
-print(
-    f"""
-Vals Eval Results
-**Pass Rate**: {summary_dict['passPercentage'] * 100:.2f}%
+# print(
+#     f"""
+# Vals Eval Results
+# **Pass Rate**: {summary_dict['passPercentage'] * 100:.2f}%
 
-**Error Case Summary**: {summary_dict['textSummary']}
+# **Error Case Summary**: {summary_dict['textSummary']}
 
-_View full results at: {run_link}_
-"""
-)
+# _View full results at: {run_link}_
+# """
+# )
